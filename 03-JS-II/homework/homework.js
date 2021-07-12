4135b39c1031b8ee4374a4ab0666bed5da25dba5
Franz <franz.zalazar@gmail.com>
2021-07-12 09:54:37 -0300

    Casi terminado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -145,6 +145,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if(num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error";
+  }else if(num1 < 0 || num2 < 0 || num3 <0){
+    return "Hay negativos";
+  }else if(num1 > num2 && num1 > num3 && num1 > 0){
+    return "Número 1 es mayor y positivo"
+  }else if(num3 > num1 && num3 > num2){
+    return num3+1;
+  }else{
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -153,12 +164,27 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero === 0 || numero === 1){
+    return false;
+  }
+  for(let i=2; i< numero; i++){
+    if(numero > 0 && numero % i != 0 && numero != 1){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor === true){
+    return "Soy verdadero";
+  }else{
+    return "Soy falso";
+  }
 
 }
 
@@ -166,12 +192,20 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var tabla;
+  for(let i = 0; i <= 10; i++){
+    tabla = tabla + i * 6;
+  }return tabla;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if(numero > 0 && numero.length === 3){
+    return true;
+  }else{
+    return false;
+  }
   
 }
 
